c2daa5ee8b477dd0d73d697911837d1527db172b
Abhranil <abhranil.blr@gmail.com>
2020-07-22 16:16:00 +0530

    Abhranil: Pushed Selenium Assignment 07 (Version 1.0) Code.

---
+++ Stackoverflow_Assng7/src/main/java/com/cucumberFramework/stepdefinitions/ServiceHooks.java
@@ -0,0 +1,29 @@
+package com.cucumberFramework.stepdefinitions;
+
+import org.apache.log4j.Logger;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.cucumberFramework.helper.LoggerHelper;
+import com.cucumberFramework.testBase.TestBase;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+
+public class ServiceHooks {
+
+	TestBase testBase;
+	Logger log = LoggerHelper.getLogger(ServiceHooks.class);
+	
+	@Before
+	public void initializeTest() {
+		testBase = new TestBase();		
+		testBase.initialization();
+	}
+
+	@After
+	public void endTest() {
+		TestBase.driver.quit();
+	}
+	
+}
---
+++ Stackoverflow_Assng7/src/main/java/com/cucumberFramework/testRunner/TestRunner.java
@@ -0,0 +1,47 @@
+package com.cucumberFramework.testRunner;
+import java.io.IOException;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import com.cucumber.listener.Reporter;
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
+
+
+
+@CucumberOptions(features = {"src/test/resources/features/login/login_logout_ValidCred.feature","src/test/resources/features/login/login_Invalidcred.feature" },
+glue = { "com/cucumberFramework/stepdefinitions" }, 
+plugin = { "pretty", "html:target/cucumber-reports/cucumber-pretty","com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/Automationreport.html",
+		"json:target/cucumber-reports/CucumberTestReport.json", "rerun:target/cucumber-reports/rerun.txt" },
+dryRun = false,monochrome = true)
+
+public class TestRunner {
+	
+	private TestNGCucumberRunner testNGCucumberRunner;
+	   
+	@BeforeClass(alwaysRun = true)
+	public void setUpClass() {
+		testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+	}
+
+	@Test(groups = "cucumber", description = "Runs cucmber Features", dataProvider = "features")
+	public void feature(CucumberFeatureWrapper cucumberFeature) {
+		testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+	}
+
+	@DataProvider
+	public Object[][] features() {
+		return testNGCucumberRunner.provideFeatures();
+	}
+
+
+	@AfterClass(alwaysRun = true)
+	public void testDownClass() throws IOException{
+		Reporter.loadXMLConfig("/Stackoverflow_Assng7/extent-config.xml");
+		testNGCucumberRunner.finish();
+	}
+
+}
